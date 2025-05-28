1921e6fe4f23e7523e9821d97254acb49a3c72e6
YudiGunzi <yudigunzi@alunos.utfpr.edu.br>
2025-05-28 17:17:47 -0300

    Corrected wrong commit

--- Entidade.h
+++ Entidade.h
@@ -1,11 +1,23 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 
 class Entidade
 {
+//o que toda entidade deve ter? sprite,
+// textura,
+// posicao inicial(algumas entidades nao vao se mover, por isso implementar apenas a posicao inicial na classe mae)
+// hitbox
+// aparecer na tela(draw)
 protected:
-
-private:
-
+	sf::RenderWindow* window;// sf::Texture& texture, int largura_sprite, int altura_sprite
+	sf::Sprite sprite;
+	sf::Texture texture;
+	sf::FloatRect hitbox;
+	sf::Vector2f init_position;
 public:
-
+	Entidade(){}
+	virtual ~Entidade(){}
+	 
+	void setWindow(sf::RenderWindow* window) { this->window = window; }
+	void draw() { window->draw(sprite); }
 };
