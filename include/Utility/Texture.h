a66cc5151fafa603f44962d38a18397070e0f9b7
Lucas T <160812489+Chico7854@users.noreply.github.com>
2025-06-05 19:39:27 -0300

    Move images/ to assets/

--- include/Utility/Texture.h
+++ include/Utility/Texture.h
@@ -12,13 +12,13 @@ namespace Texture {
         Background,
         Shuriken
     };
-        constexpr char PLAYER1[] = "../images/samurai_run.png";
-        constexpr char PLAYER2[] = "../images/shinobi_run.png";
-        constexpr char YOUKAI[] = "../images/youkai_walk.png";
-        constexpr char PLATFORM[] = "../images/platform.png";
-        constexpr char FLOOR[] = "../images/floor_mine.png";
-        constexpr char SPIKE[] = "../images/spike.png";
-        constexpr char SAW[] = "../images/saw.png";
-        constexpr char BACKGROUND[] = "../images/bgsimao.png";
-        constexpr char SHURIKEN[] = "../images/shuriken.png";
+        constexpr char PLAYER1[] = "../assets/images/samurai_run.png";
+        constexpr char PLAYER2[] = "../assets/images/shinobi_run.png";
+        constexpr char YOUKAI[] = "../assets/images/youkai_walk.png";
+        constexpr char PLATFORM[] = "../assets/images/platform.png";
+        constexpr char FLOOR[] = "../assets/images/floor_mine.png";
+        constexpr char SPIKE[] = "../assets/images/spike.png";
+        constexpr char SAW[] = "../assets/images/saw.png";
+        constexpr char BACKGROUND[] = "../assets/images/bgsimao.png";
+        constexpr char SHURIKEN[] = "../assets/images/shuriken.png";
 }
